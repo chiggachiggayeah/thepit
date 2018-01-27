5cb565913545ee144f341595e89d1ff573a312b4
chiggachiggayeah <chiggachiggayeah@users.noreply.github.com>
2018-01-27 18:20:20 -0500

    refined ECS scaffolding

---
+++ System.lua
@@ -0,0 +1,7 @@
+function _System()
+    local self = {}
+
+    
+    return self
+end
+
--- main.lua
+++ main.lua
@@ -1,28 +1,136 @@
-
 --types
-SNAKE, MONGOOSE = "SNAKE", "MONGOOSE"
+SNAKE, MONGOOSE, BUSH = "SNAKE", "MONGOOSE", "BUSH"
+ENTITY = "ENTITY"
+g_types = {SNAKE, MONGOOSE, BUSH}
+--systems
 
---entities
-function _Entity()
-    local self = {}
-    self.is_a = {"Entity"}
+function _PhysicsSystem()
+    local self = _System()
+    local registry = {}
+
+    function self.register(entity)
+        table.insert(registry, entity)  
+    end
+
+    function self.update()
+        for i, v in registry do
+        end
+    end
 
     return self
 end
 
-function Snake(...)
-    local self = _Entity()
+--rendering template
+t = {}
+t.colors = {
+    white = {"255", "255", "255", "255"} 
+}
+t[SNAKE] = function(val)
+    love.graphics.setColor(t.colors.white)
+    love.graphics.rectangle("fill", val.pos.x, val.pos.y, val.width, val.height)
+end
+--
 
-    --add all the component elements to self
-    --register with relevant systems
-    for i, c in ipairs(arg) do
-        c.register(self)
-        for k, v in pairs(c()) do
+function _RenderSystem(template)
+    local self = _System()
+    local registry = {} 
+
+    function self.register(entity)
+        table.insert(registry, entity)
+    end 
+    
+    function self.update()
+        for i, v in ipairs(registry) do
+            for k, fn in pairs(template) do
+                if v.is_a(k) then
+                    fn(v)
+                end
+            end
+        end
+    end
+
+    return self
+end
+
+RenderSystem = _RenderSystem(t)
+PhysicsSystem = _PhysicsSystem()
+
+
+--entities
+function _Entity(components)
+    local self = {}
+    self.types = {ENTITY}
+    
+    self.override = function(overrides)
+        for k, v in pairs(overrides) do
             self[k] = v
         end
     end
 
-    table.insert(self.is_a, SNAKE)
+    self.is_a = function(g_type)
+        local result = false
+        for i, v in ipairs(self.types) do
+            if(v == g_type) then
+                result = true
+                return result
+            end
+        end
+
+        return result
+    end
+
+    for i, c in ipairs(components) do
+        print("Component is: " .. tostring(c))
+        local component = c()
+        component.register(self)
+        for k, v in pairs(component) do
+            if self[k] == nil then
+                self[k] = v
+            end
+        end
+    end
+
+
+    return self
+end
+
+
+Drawable = {}
+setmetatable(Drawable, {
+    __call = function()
+        return {
+            pos = {
+                x = 0,
+                y = 0,
+            },
+            width = 50,
+            height = 50,
+            register = function(entity)
+                RenderSystem.register(entity)
+            end
+        }
+    end
+})
+
+function Snake(overrides)
+    local self = _Entity({Drawable})
+
+    --add all the component elements to self
+    --register with relevant systems
+    
+    table.insert(self.types, SNAKE)
+
+    if overrides ~= nil then
+        self.override(overrides)
+    end
+
+    return self
+end
+
+function Mongoose(...)
+    local self = _Entity(Drawable)
+
+    table.insert(self.is_a, MONGOOSE)
 
     return self
 end
@@ -64,7 +172,6 @@ Collidable = setmetatable({}, {
             },
             register = function()
             end
-
         }
     end
 })
@@ -83,19 +190,7 @@ Controllable = setmetatable({},{
     end
 })
 
-Drawable = setmetatable({}, {
-    __call = function()
-        return {
-            pos = {
-                x = 0,
-                y = 0,
-            },
-            register = function()
-            end
 
-        }
-    end
-})
 
 Perishable = setmetatable({}, {
     __call = function()
@@ -114,57 +209,19 @@ Perishable = setmetatable({}, {
 --render system
 
 --return system instances from various files so that you don't allow making additional systems
-function _System()
-    local self = {}
-
-    
-    return self
-end
-function _Physics()
-    local self = _System()
-    local registry = {}
-
-    function self.register(entity)
-        table.insert(registry, entity)  
-    end
-
-    function self.update()
-        for i, v in registry do
-        end
-    end
-end
-
-function _RenderSystem()
-    local self = _System()
-    local registry = {}
-    
-    function self.register(entity)
-        table.insert(registry, entity)
-    end function self.update()
-        for i, v in registry do
-            if v.is_a == SNAKE then
-                love.graphics.setColor(255, 255, 255, 255)
-                love.graphics.rect(v.pos.x, v.pos.y, v.width, v.height)
-            elseif v.is_a == MONGOOSE then
-            end
-        end
-    end
-end
---end systems
-
- 
 
 
 function love.load()
-
+    snake = Snake()
+    snake2 = Snake({pos = {x = 100, y = 100}})
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-    --love.graphics.print("Hello world!", 400, 300)
-
+    --love.graphics.print("Hello world!", 400, 300) 
+    RenderSystem.update()
 end
 
 
